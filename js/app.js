831e8d86f310abd0da70aec8ab562e28a9dc4e88
Yeray1412 <119167766+Yeray1412@users.noreply.github.com>
2023-04-08 00:41:48 +0200

    pp

--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@ let line = document.querySelector('.line');
 let myHeight = document.documentElement.offsetHeight - document.documentElement.clientHeight;
 
 window.addEventListener('scroll', ()=> {
-    div.style.height = `${(scrollY*8 / myHeight) * 100}vh`
+    line.style.height = `${(scrollY*8 / myHeight) * 100}vh`
 });
 
