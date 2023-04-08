46d446eedde01d444d15d3b56f5560293d006f4f
Yeray1412 <119167766+Yeray1412@users.noreply.github.com>
2023-04-08 06:14:28 +0200

    p

--- js/app.js
+++ js/app.js
@@ -5,3 +5,20 @@ window.addEventListener('scroll', ()=> {
     line.style.height = `${(scrollY*8 / myHeight) * 100}vh`
 });
 
+// swiper
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  // End swiper
